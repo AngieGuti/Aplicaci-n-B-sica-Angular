7cf15096ba29d94397ecfc5d7dd307d88ab6ed10
Angie Gutierrez <ac.gutierrezt1@uniandes.edu.co>
2024-11-05 08:21:04 -0500

    comentarios course module and component

---
+++ tutorialcourses/src/app/course/course.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core'; // import de Angular para el decorador
+import { CommonModule } from '@angular/common'; // básico de Angular (CommonModule) que todos los módulos deben importar 
+import { CourseComponent } from './course.component'; // importa el archivo del componente CourseComponent
+
+@NgModule({
+  imports: [ // valor es un arreglo de los nombres de los módulos que necesita este módulo
+    CommonModule
+  ],
+  declarations: [CourseComponent] // valor también es un arreglo de los nombres de los componentes que declara
+})  // declaración del componente que se crea por defecto al crear un módulo.
+export class CourseModule { }
