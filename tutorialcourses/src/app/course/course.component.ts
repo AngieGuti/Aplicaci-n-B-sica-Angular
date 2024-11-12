1456e5d68f5b03ef18d1b978a6529d66c1e003a3
Angie Gutierrez <ac.gutierrezt1@uniandes.edu.co>
2024-11-11 19:56:36 -0500

    servicio

--- tutorialcourses/src/app/course/course.component.ts
+++ tutorialcourses/src/app/course/course.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Course } from './course';
+import { dataCourses } from './dataCourses';
+import { CourseService } from './course.service';
 
 @Component({
   selector: 'app-course',
@@ -6,10 +9,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./course.component.css']
 })
 export class CourseComponent implements OnInit {
+  constructor(private courseService: CourseService) { }
+  courses: Array<Course>= [];
 
-  constructor() { }
-
-  ngOnInit() {
+  getCourses() {
+    this.courseService.getCourses().subscribe(courses => {
+      this.courses = courses;
+    });
   }
-
+  ngOnInit() {
+    this.getCourses();
+  }
 }
