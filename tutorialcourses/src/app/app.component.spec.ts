fa125748434e8001056425b3fc556752b6307ef7
Angie Gutierrez <ac.gutierrezt1@uniandes.edu.co>
2024-11-11 20:47:23 -0500

    me falla una prueba

--- tutorialcourses/src/app/app.component.spec.ts
+++ tutorialcourses/src/app/app.component.spec.ts
@@ -1,15 +1,18 @@
 import { TestBed } from '@angular/core/testing';
-import { RouterModule } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { CourseComponent } from './course/course.component';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        RouterModule.forRoot([])
+        RouterTestingModule, HttpClientModule
       ],
       declarations: [
-        AppComponent
+        AppComponent,
+        CourseComponent
       ],
     }).compileComponents();
   });
@@ -24,12 +27,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('tutorialcourses');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, tutorialcourses');
   });
 });
--- tutorialcourses/src/app/course/course.service.spec.ts
+++ tutorialcourses/src/app/course/course.service.spec.ts
@@ -1,16 +1,18 @@
 /* tslint:disable:no-unused-variable */
 
-import { TestBed, async, inject } from '@angular/core/testing';
+import { TestBed, inject } from '@angular/core/testing';
 import { CourseService } from './course.service';
+import { HttpClientTestingModule } from "@angular/common/http/testing";
 
 describe('Service: Course', () => {
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [CourseService]
-    });
-  });
+ beforeEach(() => {
+   TestBed.configureTestingModule({
+     imports: [HttpClientTestingModule],
+     providers: [CourseService]
+   });
+ });
 
-  it('should ...', inject([CourseService], (service: CourseService) => {
-    expect(service).toBeTruthy();
-  }));
+ it('should ...', inject([CourseService], (service: CourseService) => {
+   expect(service).toBeTruthy();
+ }));
 });
